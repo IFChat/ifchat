5dacede95c0088bccf72205a84762f5803afe1a1
IFChat <ifchat.ifsc@gmail.com>
2021-02-10 12:21:00 -0300

    Revert "Revert "Implementação do Login""
    This reverts commit 1ec355d095381999f795517128efc02f9b561324.

--- API.js
+++ API.js
@@ -2,26 +2,61 @@ import firebase from "./database"
 
 const api = {
 
-    findUserByName: async function(name){
-        const user = await firebase
-            .database()
-            .ref('users')
-            .orderByChild('name')
-            .equalTo(name)
-            .once('value')
-
-
-        if (user.exists()){
-            return Object.values(user.val()[0])
-        }
-        return null;
+    createUserinDatabase: function(user){
+        firebase.database().ref('users').push(user)
+        .then(function(user){
+            console.log('Sucesso na criação fdp');
+        })
+    },
+    
+    currentUser: function(user){
+        firebase.auth().currentUser;
     },
 
-    createUser: function (user){
-        firerbase.database().ref('users').push(user);
+
+    loginUser: function (email, password){
+                firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(function(user){
+                    console.log('Autenticado com sucesso singin!');
+                })
+    },
+
+    createUser: function (email, password){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(user){
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                console.log(error.code);
+                console.log(error.message);
+                console.log('Erro de Autenticação!');
+            });
+        })
+        .catch(function(error){
+            console.log(error.code);
+            console.log(error.message);
+            console.log('Erro na Criação de conta!');
+        })
     }
+
+
 };
 
+
+
+/*findUserByName: async function(name){
+    const user = await firebase
+        .database()
+        .ref('users')
+        .orderByChild('name')
+        .equalTo(name)
+        .once('value')
+
+
+    if (user.exists()){
+        return Object.values(user.val()[0])
+    }
+    return null;
+},*/
 
 
 export default api;
